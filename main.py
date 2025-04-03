5d9426fa3c438a5cdf14088c27b3994836529892
Konstantin Berezin <berezink90@mail.ru>
2025-04-03 19:54:34 +0500

    del two func

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "This is the about page."
-
-@app.route("/contact")
-def contact():
-    return "This is the contact page"
 
 if __name__ == "__main__":
     app.run(debug=True)
